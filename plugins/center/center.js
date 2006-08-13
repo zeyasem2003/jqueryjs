36fb6e59f6b6052e851794dd9990753f6b9850d3
john <john@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-08-13 17:34:25 +0000

    Updated the center plugin to be current.

--- plugins/center/center.js
+++ plugins/center/center.js
@@ -2,25 +2,20 @@
  * Takes all matched elements and centers them, absolutely, 
  * within the context of their parent element. Great for 
  * doing slideshows.
- *   $("div img").center();
+ *
+ * @example $("div img").center();
+ * @name center
+ * @type jQuery
  */
 $.fn.center = function(f) {
 	return this.each(function(){
-		if ( !f && this.nodeName == 'IMG' &&
-				 !this.offsetWidth && !this.offsetHeight ) {
-			var self = this;
-			setTimeout(function(){
-				$(self).center(true);
-			}, 13);
-		} else {
-			var s = this.style;
-			var p = this.parentNode;
-			if ( $.css(p,"position") == 'static' ) {
-				p.style.position = 'relative';
-			}
-			s.position = 'absolute';
-			s.left = (($.css(p,"width") - $.css(this,"width"))/2) + "px";
-			s.top = (($.css(p,"height") - $.css(this,"height"))/2) + "px";
-		}
-  });
+		var p = this.parentNode;
+		if ( $.css(p,"position") == 'static' )
+			p.style.position = 'relative';
+
+		var s = this.style;
+		s.position = 'absolute';
+		s.left = ((parseInt($.css(p,"width")) - parseInt($.css(this,"width")))/2) + "px";
+		s.top = ((parseInt($.css(p,"height")) - parseInt($.css(this,"height")))/2) + "px";
+	});
 };

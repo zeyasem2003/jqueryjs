9a6df67a51ec5d4836f395542f8385eb8e9e95ac
john <john@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-09-11 06:10:03 +0000

    Improved the docs for the form plugin (got it "up to spec"), also fixed up the serialize function to be more useful.

--- plugins/form/form.js
+++ plugins/form/form.js
@@ -9,54 +9,53 @@
  * There are three different resulting operations that can occur, after
  * your form has been submitted.
  *
- * 1. The form is submitted and a callback is fired, letting you know
- *    when it's done:
- *    $("form").ajaxSubmit(function(){
- *      alert("all done!");
- *    });
- *
- * 2. The form is submitted and the resulting HTML contents are injected
- *    into the page, at your specified location.
- *    $("form").ajaxSubmit("#destination");
- *
- * 3. The form is submitted and the results returned from the server are
- *    automatically executed (useful for having the server return more
- *    Javascript commands to execute).
- *    $("form").ajaxSubmit();
- *
  * Additionally, an optional pre-submit callback can be provided. If it,
  * when called with the contents of the form, returns false, the form will
  * not be submitted.
  *
- * Finally, both the URL and method of the form submission can be
- * overidden using the 'url' and 'mth' arguments.
+ * @example $("form").ajaxSubmit(function(){
+ *   alert("all done!");
+ * });
+ * @desc The form is submitted and a callback is fired, letting you know when it's done.
  *
+ * @example $("form").ajaxSubmit("#destination");
+ * @desc The form is submitted and the resulting HTML contents are injected into the page, at your specified location.
+ *
+ * @example $("form").ajaxSubmit();
+ * @desc The form is submitted and the results returned from the server are
+ * automatically executed (useful for having the server return more Javascript commands to execute).
+ *
+ * @name ajaxSubmit
+ * @type jQuery
  * @param target   arg for the target id element to render
  * @param post_cb  callback after any results are returned
  * @param pre_cb   callback function before submission
- * @param url      form action override
- * @param mth      form method override
- * @return         "this" object
- * @see            ajaxForm(), serialize(), load(), $.ajax()
- * @author         Mark Constable (markc@renta.net)
- * @author         G. vd Hoven, Mike Alsup, Sam Collett, John Resig
+ * @return jQuery 
+ * @see ajaxForm
+ * @see load
+ * @see $.ajax
+ * @author Mark Constable (markc@renta.net)
+ * @author G. vd Hoven
+ * @author Mike Alsup
+ * @author Sam Collett
+ * @author John Resig
  */
 $.fn.ajaxSubmit = function(target, post_cb, pre_cb, url, mth) {
-	if ( !this.vars ) this.serialize();
+	var vars = this.serialize();
 	
-	if (pre_cb && pre_cb.constructor == Function && pre_cb(this.vars) === false) return;
+	if (pre_cb && pre_cb.constructor == Function && pre_cb(vars) === false) return;
 
 	var f = this.get(0);
 	var url = url || f.action || '';
 	var mth = mth || f.method || 'POST';
 
 	if (target && target.constructor == Function)
-		$.ajax(mth, url, $.param(this.vars), target);
+		$.ajax(mth, url, $.param(vars), target);
 	else if (target && target.constructor == String)
-		$(target).load(url, this.vars, post_cb);
+		$(target).load(url, vars, post_cb);
 	else {
-		this.vars.push({name: 'evaljs', value: 1});
-		$.ajax(mth, url, $.param(this.vars), function(r) {
+		vars.push({name: 'evaljs', value: 1});
+		$.ajax(mth, url, $.param(vars), function(r) {
 			eval(r.responseText);
 		});
 	}
@@ -65,48 +64,49 @@ $.fn.ajaxSubmit = function(target, post_cb, pre_cb, url, mth) {
 };
 
 /**
- * This function can be used to turn any HTML form into a form
- * that submits using AJAX only.
+ * Turn any HTML form into a form that submits using AJAX only.
  *
- * The purpose of using this method, instead of the ajaxSubmit()
+ * The advantage of using this method, instead of the ajaxSubmit()
  * and submit() methods, is to make absolutely sure that the
  * coordinates of <input type="image"/> elements are transmitted
  * correctly OR figuring out exactly which <input type="submit"/>
  * element was clicked to submit the form.
  *
- * If neither of the above points are important to you, then you'll
- * probably just want to stick with the simpler ajaxSubmit() function.
+ * If neither of the above points are important to you, then you
+ * may want to stick with the ajaxSubmit() function.
  *
- * Usage examples, similar to ajaxSubmit():
+ * @example $('#form-id').ajaxForm();
+ * @desc Just eval the results returned from the backend.
  *
- * 1. Just eval the results returned from the backend.
- *    $('#form-id').ajaxForm();
+ * @example $('#form-id').ajaxForm('#target-id');
+ * @desc Render backend results directly to target ID (expects (x)HTML).
  *
- * 2. Render backend results directly to target ID (expects (x)HTML).
- *    $('#form-id').ajaxForm('#target-id');
+ * @example $('#form-id').ajaxForm(post_callback);
+ * @desc Submit to backend URL (form action) then call this function.
  *
- * 3. Submit to backend URL (form action) then call this function.
- *    $('#form-id').ajaxForm(post_callback);
+ * @example $('#form-id').ajaxForm('#target-id', post_callback);
+ * @desc Load target ID with backend results then call a function.
  *
- * 4. Load target ID with backend results then call a function.
- *    $('#form-id').ajaxForm('#target-id', post_callback);
+ * @example $('#form-id').ajaxForm('#target-id', null, pre_callback);
+ * @desc Call a browser function (for validation) and then (optionally) load server results to target ID.
  *
- * 5. Call a browser function (for validation) and then (optionally)
- *    load server results to target ID.
- *    $('#form-id').ajaxForm('#target-id', null, pre_callback);
+ * @example $('#form-id').ajaxForm('#target-id', post_callback, pre_callback);
+ * @desc Call validation function first then load server results to target ID and then also call a browser function.
  *
- * 6. Call validation function first then load server results to
- *    target ID and then also call a browser function.
- *    $('#form-id').ajaxForm('#target-id', post_callback, pre_callback);
- *
+ * @name ajaxForm
  * @param target   arg for the target id element to render
  * @param post_cb  callback after any results are returned
  * @param pre_cb   callback function before submission
- * @return         the jQuery Object
+ * @return jQuery
  * @type jQuery
- * @see            serialize(), ajaxSubmit()
- * @author         Mark Constable (markc@renta.net)
- * @author         G. vd Hoven, Mike Alsup, Sam Collett, John Resig
+ * @see serialize
+ * @see submit
+ * @see ajaxSubmit
+ * @author Mark Constable (markc@renta.net)
+ * @author G. vd Hoven
+ * @author Mike Alsup
+ * @author Sam Collett
+ * @author John Resig
  */
 $.fn.ajaxForm = function(target, post_cb, pre_cb) {
 	return this.each(function(){
@@ -127,48 +127,29 @@ $.fn.ajaxForm = function(target, post_cb, pre_cb) {
 };
 
 /**
- * A simple wrapper function that sits around the .serialize()
- * method, allowing you to easily extract the data stored within
- * a form.
+ * This function gathers form element variables into an array of objects
+ * that can be passed to any AJAX function (such as $.post, $.ajax, or load).
  *
- * Usage examples:
- *
- * 1. Serialize the contents of a form to a & and = delmited string:
- *    $.param( $("form").formdata() );
- *
- * @return         An array of name/value pairs representing the form
- * @type Array<Object>
- * @see            serialize()
- # @author         John Resig
- */
-$.fn.formdata = function(){
-	this.serialize();
-	return this.vars;
-};
-
-/**
- * This function gathers form element variables into an array that
- * is embedded into the current "this" variable as "this.vars". It
- * is normally used in conjunction with formdata() or ajaxSubmit() but can
+ * This function is primarily used by ajaxSubmit() and ajaxForm(), but can
  * be used standalone as long as you don't need the x and y coordinates
- * associated with an <input type="image"/> element..
+ * associated with an <input type="image"/> elements.
  *
- * Standalone usage examples:
+ * @example var vars = $("#myForm").serialize();
+ * $.post( "myscript.cgi", vars );
+ * @desc Collect all the data from a form and submit it to a server-side application.
  *
- * 1. Gather form vars and return array to LHS variable.
- *    var myform = $('#form-id').serialize();
- *
- * 2. Provide a serialized URL-ready string (after 1. above).
- *    var mystring = $.param(myform.vars);
- *
- * 3. Gather form vars and send to RHS plugin via "this.vars".
- *    $('#form-id').serialize().some_other_plugin();
- *
- * @return         the jQuery Object
- * @return jQuery
- * @see            ajaxForm(), ajaxSubmit()
- * @author         Mark Constable (markc@renta.net)
- * @author         G. vd Hoven, Mike Alsup, Sam Collett, John Resig
+ * @name serialize
+ * @type Array<Object>
+ * @see ajaxForm
+ * @see ajaxSubmit
+ * @see $.ajax
+ * @see $.post
+ * @see $.get
+ * @author Mark Constable (markc@renta.net)
+ * @author G. vd Hoven
+ * @author Mike Alsup
+ * @author Sam Collett
+ * @author John Resig
  */
 $.fn.serialize = function() {
 	var a = [];
@@ -193,8 +174,6 @@ $.fn.serialize = function() {
 
 		a.push({name: n, value: this.value});
 	});
-	
-	this.vars = a;
 
-	return this;
+	return a;
 };

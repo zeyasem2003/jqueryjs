71a64a2ac364dc98ae4d068ca5a08dfc2544a97a
klaus <klaus@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-10-04 19:53:47 +0000

    tabs plugin 1.2

--- plugins/tabs/tabs.js
+++ plugins/tabs/tabs.js
@@ -1,5 +1,5 @@
- // tabs - jQuery plugin for accessible, unobtrusive tabs by Klaus Hartl
-// v 1.1
+// tabs - jQuery plugin for accessible, unobtrusive tabs by Klaus Hartl
+// v 1.2
 // http://stilbuero.de/tabs/
 // Free beer and free speech. Enjoy!
 $.fn.tabs = function(options) {
@@ -33,14 +33,45 @@ $.fn.tabs = function(options) {
         $(this).find('>div').not(':eq(' + on + ')').addClass(OFF_CLASS);
         $(this).find('>ul>li:eq(' + on + ')').addClass(ON_CLASS);
         var container = this;
+        var undefined;
         $(this).find('>ul>li>a').click(function() {
             if (!$(this.parentNode).is('.' + ON_CLASS)) {
                 var target = $('#' + re.exec(this.href)[1]);
                 if (target.size() > 0) {
-                    $(container).find('>div:visible').addClass(OFF_CLASS);
-                    target.removeClass(OFF_CLASS);
-                    $(container).find('>ul>li').removeClass(ON_CLASS);
-                    $(this.parentNode).addClass(ON_CLASS);
+                    var self = this;
+                    var visible = $(container).find('>div:visible');
+                    if (options && options.slide && options.fade) {
+                        visible.animate({height: 'hide', opacity: 'hide'}, options.slide, function() {
+                            // TODO check accessibility for fade
+                            //$(this).addClass(OFF_CLASS).css({display: '', height: 'auto'}); // retain acccessibility for print and other media types
+                            $(container).find('>ul>li').removeClass(ON_CLASS);
+                            $(self.parentNode).addClass(ON_CLASS);
+                            //target.css('display', 'none').removeClass(OFF_CLASS).animate({height: 'show', opacity: 'show'}, options.slide);
+                            target.animate({height: 'show', opacity: 'show'}, options.slide);
+                        });
+                    } else if (options && options.slide) {
+                        visible.slideUp(options.slide, function() {
+                            //$(this).addClass(OFF_CLASS).css({display: '', height: 'auto'}); // retain acccessibility for print and other media types
+                            $(container).find('>ul>li').removeClass(ON_CLASS);
+                            $(self.parentNode).addClass(ON_CLASS);
+                            //target.css('display', 'none').removeClass(OFF_CLASS).slideDown(options.slide);
+                            target.slideDown(options.slide);
+                        });
+                    } else if (options && options.fade) {
+                        visible.fadeOut(options.fade, function() {
+                            // TODO check accessibility for fade
+                            //$(this).addClass(OFF_CLASS).css({display: '', opacity: '1'}); // retain acccessibility for print and other media types
+                            $(container).find('>ul>li').removeClass(ON_CLASS);
+                            $(self.parentNode).addClass(ON_CLASS);
+                            //target.css('display', 'none').removeClass(OFF_CLASS).fadeIn(options.fade);
+                            target.fadeIn(options.fade);
+                        });
+                    } else {
+                        visible.addClass(OFF_CLASS);
+                        $(container).find('>ul>li').removeClass(ON_CLASS);
+                        $(this.parentNode).addClass(ON_CLASS);
+                        target.removeClass(OFF_CLASS);
+                    }
                 } else {
                     alert('There is no such container.');
                 }

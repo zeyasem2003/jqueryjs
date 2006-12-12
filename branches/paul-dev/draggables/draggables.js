59b107d82f8776f94b195a0013435cd4d75b5d68
paul <paul@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-12-12 14:01:44 +0000

    removed some things not needed..

--- branches/paul-dev/draggables/draggables.js
+++ branches/paul-dev/draggables/draggables.js
@@ -10,24 +10,22 @@
 		oldPosition: null,
 		helper: null,
 		init: function(o) {
-			
+
 			if(!o)
 				var o = {};
 
-			return this.each(function() {
-				
+			return this.each(function() {			
+
 				/* Prepare the options */
 				f.manager[this] = {
 					handle : o.handle ? ($(o.handle, this).get(0) ? $(o.handle, this) : $(this)) : $(this),
 					onStart : o.onStart && o.onStart.constructor == Function ? o.onStart : false,
 					onStop : o.onStop && o.onStop.constructor == Function ? o.onStop : false,
 					onDrag : o.onDrag && o.onDrag.constructor == Function ? o.onDrag : false,
-					cursor: o.cursor ? o.cursor : null,
 					helper: o.helper ? o.helper : ['',''],
 					dragPrevention: o.dragPrevention ? o.dragPrevention : 0,
 					dragPreventionOn: o.dragPreventionOn ? o.dragPreventionOn.toLowerCase().split(",") : ["input","textarea","button"],
-					containment: o.containment ? o.containment : null,
-					cursorAt: { top: -5, left: -5 },
+					cursorAt: { top: ((o.cursorAt && o.cursorAt.top && o.cursorAt.top < 0) ? o.cursorAt.top : -1), left: ((o.cursorAt && o.cursorAt.left && o.cursorAt.left < 0) ? o.cursorAt.left : -1) },
 					init: false
 				};
 
@@ -73,18 +71,14 @@
 		evStart: function(e) {
 
 			var o = f.manager[f.current];
-			
-			/* Set cursor on body if found in options */
-			if(o.cursor)
-				$("html").css("cursor", o.cursor);
 
 			/* Get the current mouse position */
 			f.position = (e.pageX) ? [e.pageX,e.pageY] : [e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,e.clientY + document.body.scrollTop + document.documentElement.scrollTop];
 				
-			/* Append the helper div */
+			/* Append the helper div or clone */
 			f.helper = document.createElement("div");
 			$(f.helper).html((o.helper[1] ? o.helper[1] : '')).attr("class", o.helper[0]).css("position", "absolute").css("left", f.position[0]-(o.cursorAt.left ? o.cursorAt.left : 0)+"px").css("top", f.position[1]-(o.cursorAt.top ? o.cursorAt.top : 0)+"px").appendTo("body");
-
+			
 			/* Okay, initialization is done, then set it to true */
 			o.init = true;			
 			
@@ -95,37 +89,25 @@
 		},
 		evStop: function(e) {			
 
+			var o = f.manager[f.current];
+
+			/* Unbind the mouse events */
 			$(document).unbind("mouseup");
 			$(document).unbind("mousemove");
 			
-			/* If init is false, don't do the following, just set
-			 * properties to null
-			 */
-			if(f.manager[f.current].init == false) {
-				f.current = null;
-				f.oldPosition = null;
-				f.position = null;
-				return;
-			}
-			
-			var o = f.manager[f.current];
-
-			/* Remove cursor on body if it was set before */
-			if(o.cursor)
-				$("html").css("cursor", "");
+			/* If init is false, don't do the following, just set properties to null */
+			if(f.manager[f.current].init == false)
+				return f.current = f.oldPosition = f.position = null;
 			
 			/* Trigger the onStop callback */
 			if(f.manager[f.current].onStop)
 				f.manager[f.current].onStop.apply(f.current);
 				
-			f.manager[f.current].init = false;
-			f.current = null;
-			f.oldPosition = null;
-			f.position = null;
-			
 			/* Remove helper */
 			$("body").get(0).removeChild(f.helper);
-			f.helper = null;
+
+			f.manager[f.current].init = false;
+			f.current = f.oldPosition = f.position = f.helper = null;
 				
 		},
 		evDrag: function(e) {
@@ -144,16 +126,7 @@
 			
 			/* Trigger the onDrag callback */
 			if(f.manager[f.current].onDrag)
-				var dragRet = f.manager[f.current].onDrag.apply(f.current, [f.helper,f.position[0],f.position[1]]);
-			/* If something came back from the callback, use the new x,y coords */
-			if(typeof dragRet == "object") f.position[dragRet[0],dragRet[1]];
-			
-				
-			/* Let's see if we have a contaiment */
-			if(o.containment == "document") {
-				if(Math.max(document.body.scrollWidth, (self.innerWidth || jQuery.boxModel && document.documentElement.clientWidth || document.body.clientWidth)) - f.position[0] - f.helper.offsetWidth + (o.cursorAt.left ? o.cursorAt.left : 0) <= 0) { $(f.helper).css("top", f.position[1]-(o.cursorAt.top ? o.cursorAt.top : 0)+"px"); return; }; 	
-				if(Math.max(document.body.scrollHeight, (self.innerHeight || jQuery.boxModel && document.documentElement.clientHeight || document.body.clientHeight)) - f.position[1] - f.helper.offsetHeight + (o.cursorAt.top ? o.cursorAt.top : 0) <= 0)  { $(f.helper).css("left", f.position[0]-(o.cursorAt.left ? o.cursorAt.left : 0)+"px"); return; }; 	
-			}
+				f.manager[f.current].onDrag.apply(f.current, [f.helper,f.position[0],f.position[1]]);		
 				
 			/* Stick the helper to the cursor or to modified x/y */			
 			$(f.helper).css("left", f.position[0]-(o.cursorAt.left ? o.cursorAt.left : 0)+"px").css("top", f.position[1]-(o.cursorAt.top ? o.cursorAt.top : 0)+"px");

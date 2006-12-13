562cd5f2eb1d7b7c4261ed4354199bcb4d5e024e
paul <paul@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-12-13 16:02:42 +0000

    removed the dropHelper again, added its functionality to onOut: if no onOut specified, it will revert to the previous old html

--- branches/paul-dev/draggables/draggables.js
+++ branches/paul-dev/draggables/draggables.js
@@ -11,14 +11,16 @@
 				var o = {};	
 			
 			return this.each(function() {
-				this.options = {
+				this.dropOptions = {
 					accept: o.accept && o.accept.constructor == Function ? o.accept : function(dragEl) {
 						return dragEl.className.match(new RegExp('(\\s|^)'+o.accept+'(\\s|$)'));	
 					},
 					onHover : o.onHover && o.onHover.constructor == Function ? o.onHover : false,
-					onOut : o.onOut && o.onOut.constructor == Function ? o.onOut : false,
-					onDrop : o.onDrop && o.onDrop.constructor == Function ? o.onDrop : false,
-					onHoverHelper : o.onHoverHelper || false 
+					onOut : o.onOut && o.onOut.constructor == Function ? o.onOut : function(drag,helper) {
+						console.info(helper.oldContent);
+						$(helper).html(helper.oldContent);						
+					},
+					onDrop : o.onDrop && o.onDrop.constructor == Function ? o.onDrop : false 
 				}
 
 				/* Bind the hovering events */
@@ -34,30 +36,26 @@
 		},
 		evHover: function(e) {
 
-			var o = this.options;
-			
+			var o = this.dropOptions;
+
+			/* Save the old content of the helper */
+			if(f.current && o.accept(f.current)) f.helper.oldContent = $(f.helper).html();			
+
 			/* Fire the callback if we are dragging and the accept function returns true */
 			if(f.current && o.onHover && o.accept(f.current)) o.onHover.apply(this, [f.current, f.helper]);
-			if(f.current && o.onHoverHelper && o.accept(f.current)) {
-				f.helper.oldContent =$(f.helper).html();
-				$(f.helper).html(o.onHoverHelper);
-			}
 
 		},
 		evOut: function(e) {
 
-			var o = this.options;
+			var o = this.dropOptions;
 			
 			/* Fire the callback if we are dragging and the accept function returns true */
 			if(f.current && o.onOut && o.accept(f.current)) o.onOut.apply(this, [f.current, f.helper]);	
-			if(f.current && o.onHoverHelper && o.accept(f.current)) {
-				$(f.helper).html(f.helper.oldContent);
-			}	
 
 		},
 		evDrop: function(e) {
 
-			var o = this.options;
+			var o = this.dropOptions;
 			
 			/* Fire the callback if we are dragging and the accept function returns true */
 			if(f.current && o.onDrop && o.accept(f.current)) o.onDrop.apply(this, [f.current, f.helper]);			
@@ -79,7 +77,7 @@
 			return this.each(function() {			
 
 				/* Prepare the options */
-				f.manager[this] = {
+				this.dragOptions = {
 					handle : o.handle ? ($(o.handle, this).get(0) ? $(o.handle, this) : $(this)) : $(this),
 					onStart : o.onStart && o.onStart.constructor == Function ? o.onStart : false,
 					onStop : o.onStop && o.onStop.constructor == Function ? o.onStop : false,
@@ -92,10 +90,10 @@
 				};
 
 				/* Bind the mousedown event */
-				f.manager[this].handle.bind("mousedown", f.evClick);
+				this.dragOptions.handle.bind("mousedown", f.evClick);
 				
 				/* Link the original element to the handle for later reference */
-				f.manager[this].handle.get(0).dragEl = this;
+				this.dragOptions.handle.get(0).dragEl = this;
 				
 				/* Prevent text selection */
 				if($.browser.mozilla){
@@ -117,8 +115,8 @@
 
 			/* Prevent execution on defined elements */
 			var targetName = (e.target) ? e.target.nodeName.toLowerCase() : e.srcElement.nodeName.toLowerCase();
-			for(var i=0;i<f.manager[this.dragEl].dragPreventionOn.length;i++) {
-				if(targetName == f.manager[this.dragEl].dragPreventionOn[i]) return;
+			for(var i=0;i<this.dragEl.dragOptions.dragPreventionOn.length;i++) {
+				if(targetName == this.dragEl.dragOptions.dragPreventionOn[i]) return;
 			}
 		
 			/* Set f.current to the current element */
@@ -135,7 +133,7 @@
 		},
 		evStart: function(e) {
 
-			var o = f.manager[f.current];
+			var o = f.current.dragOptions;
 
 			/* Get the current mouse position */
 			f.position = (e.pageX) ? [e.pageX,e.pageY] : [e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,e.clientY + document.body.scrollTop + document.documentElement.scrollTop];
@@ -159,30 +157,30 @@
 		},
 		evStop: function(e) {			
 
-			var o = f.manager[f.current];
+			var o = f.current.dragOptions;
 
 			/* Unbind the mouse events */
 			$(document).unbind("mouseup");
 			$(document).unbind("mousemove");
 			
 			/* If init is false, don't do the following, just set properties to null */
-			if(f.manager[f.current].init == false)
+			if(o.init == false)
 				return f.current = f.oldPosition = f.position = null;
 			
 			/* Trigger the onStop callback */
-			if(f.manager[f.current].onStop)
-				f.manager[f.current].onStop.apply(f.current);
+			if(o.onStop)
+				o.onStop.apply(f.current);
 				
 			/* Remove helper */
 			$("body").get(0).removeChild(f.helper);
 
-			f.manager[f.current].init = false;
+			o.init = false;
 			f.current = f.oldPosition = f.position = f.helper = null;
 				
 		},
 		evDrag: function(e) {
 
-			var o = f.manager[f.current];
+			var o = f.current.dragOptions;
 		
 			/* Get the current mouse position */
 			f.position = (e.pageX) ? [e.pageX,e.pageY] : [e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft,e.clientY + document.body.scrollTop + document.documentElement.scrollTop];
@@ -191,12 +189,12 @@
 			if( (Math.abs(f.position[0]-f.oldPosition[0]) > o.dragPrevention || Math.abs(f.position[1]-f.oldPosition[1]) > o.dragPrevention) && o.init == false)
 				f.evStart.apply(f.current,[e]);			
 			else {
-				if(f.manager[f.current].init == false) return;
+				if(o.init == false) return;
 			}
 			
 			/* Trigger the onDrag callback */
-			if(f.manager[f.current].onDrag)
-				f.manager[f.current].onDrag.apply(f.current, [f.helper,f.position[0],f.position[1]]);		
+			if(o.onDrag)
+				o.onDrag.apply(f.current, [f.helper,f.position[0],f.position[1]]);		
 				
 			/* Stick the helper to the cursor or to modified x/y */			
 			$(f.helper).css("left", f.position[0]-(o.cursorAt.left ? o.cursorAt.left : 0)+"px").css("top", f.position[1]-(o.cursorAt.top ? o.cursorAt.top : 0)+"px");

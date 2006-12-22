fe9cd9762cc8f49397547c50208f420921b21f6f
paul <paul@c715fcbe-d12f-0410-84c4-316a508785bb>
2006-12-22 13:51:08 +0000

    added content area

--- branches/paul-dev/newpage/behaviour.js
+++ branches/paul-dev/newpage/behaviour.js
@@ -3,14 +3,26 @@
  */
 $(document).ready(function() {
 	$("#bottombar ul li").hover(function() {
-		$("img.pfeil", this).animate({ width: 10, marginLeft: -5, height: 71, top: -85 }, 1000);
+		$("img.pfeil", this).animate({ width: 10, marginLeft: -5, height: 54, top: -69 }, 1000);
 		$("div.carousel", this).animate({ height: 140, top: -140, opacity: 'show' }, 1000);
 		
 	}, function() {
-		$("img.pfeil", this).animate({ width: 47, marginLeft: -24, height: 31, top: -45, opacity: 'hide' }, 1000);
+		$("img.pfeil", this).animate({ width: 46, marginLeft: -24, height: 24, top: -39, opacity: 'hide' }, 1000);
 		$("div.carousel", this).animate({ height: 1, top: -40, opacity: 'hide' }, 1000);
 	});
-
+	
+	$("#bottombar ul li a").click(function() {
+		$(this).TransferTo({
+    		duration: 500,
+    		to: 'content', 
+    		className: 'transferIndicator'
+		});
+	});
+	
+	/* Style the content via js */
+	//$("#content").css("border", "2px solid #fff").css("position", "absolute").
+	//css("top", "10px").css("left", "10px").css("width", $(window).width()-4-20+"px").
+	//css("height", $(window).height()-4-20+"px");
 });
 
 $(window).bind("load", function() {

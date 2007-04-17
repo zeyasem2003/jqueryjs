5f449f770bef4ced026736278ef4183bfaeb3576
kelvin <kelvin@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-04-17 17:08:31 +0000

    Continued work on datePicker and getting the basics of the popup calendar in place.

--- branches/kelvin-dev/plugins/datePicker/jquery.datePicker.js
+++ branches/kelvin-dev/plugins/datePicker/jquery.datePicker.js
@@ -7,17 +7,6 @@
  **/
 
 (function($){
-
-	$.datePicker = {
-		/* constants */
-		SHOW_HEADER_NONE	:	0,
-		SHOW_HEADER_SHORT	:	1,
-		SHOW_HEADER_LONG	:	2,
-		/* locale */
-		locale      :   {
-			firstDayOfWeek      :   1
-		}
-	}
     
 	$.fn.extend({
 		/**
@@ -27,29 +16,28 @@
 		 * @option Number month The month to render (NOTE that months are zero based). Default is today's month.
 		 * @option Number year The year to render. Default is today's year.
 		 * @option Function renderCallback A reference to a function that is called as each cell is rendered and which can add classes and event listeners to the created nodes. Default is no callback.
-		 * @option Number showHeader Whether or not to show the header row, possible values are: SHOW_HEADER_NONE (no header), SHOW_HEADER_SHORT (first letter of each day) and SHOW_HEADER_LONG (full name of each day). Default is SHOW_HEADER_SHORT.
+		 * @option Number showHeader Whether or not to show the header row, possible values are: $.dpConst.SHOW_HEADER_NONE (no header), $.dpConst.SHOW_HEADER_SHORT (first letter of each day) and $.dpConst.SHOW_HEADER_LONG (full name of each day). Default is $.dpConst.SHOW_HEADER_SHORT.
 		 **/
 		renderCalendar  :   function(s)
 		{
 			s = $.extend(
 				{
-					month           : null,
+					month			: null,
 					year			: null,
-					renderCallback  : null,
-					showHeader		: $.datePicker.SHOW_HEADER_SHORT
+					renderCallback	: null,
+					showHeader		: $.dpConst.SHOW_HEADER_SHORT,
+					dpController	: null
 				}
 				, s
 			);
 			
-			var locale = $.datePicker.locale;
-			
-			if (s.showHeader != $.datePicker.SHOW_HEADER_NONE) {
+			if (s.showHeader != $.dpConst.SHOW_HEADER_NONE) {
 				var headRow = $('<tr></tr>');
-				for (var i=locale.firstDayOfWeek; i<locale.firstDayOfWeek+7; i++) {
+				for (var i=Date.firstDayOfWeek; i<Date.firstDayOfWeek+7; i++) {
 					var weekday = i%7;
 					var day = Date.dayNames[weekday];
 					headRow.append(
-						jQuery("<th></th>").attr({'scope':'col', 'abbr':day, 'title':day, 'class':(weekday == 0 || weekday == 6 ? 'weekend' : 'weekday')}).html(s.showHeader == $.datePicker.SHOW_HEADER_SHORT ? day.substr(0, 1) : day)
+						jQuery("<th></th>").attr({'scope':'col', 'abbr':day, 'title':day, 'class':(weekday == 0 || weekday == 6 ? 'weekend' : 'weekday')}).html(s.showHeader == $.dpConst.SHOW_HEADER_SHORT ? day.substr(0, 1) : day)
 					);
 				}
 			}
@@ -62,7 +50,7 @@
 										}
 									)
 									.append(
-										(s.showHeader != $.datePicker.SHOW_HEADER_NONE ? 
+										(s.showHeader != $.dpConst.SHOW_HEADER_NONE ? 
 											$("<thead></thead>")
 												.append(headRow)
 											:
@@ -71,11 +59,7 @@
 									);
 			var tbody = $('<tbody></tbody>');
 			
-			var today = new Date();
-			today.setHours(0);
-			today.setMinutes(0);
-			today.setSeconds(0);
-			today.setMilliseconds(0);
+			var today = (new Date()).zeroTime();
 			
 			var month = s.month == undefined ? today.getMonth() : s.month;
 			var year = s.year || today.getFullYear();
@@ -83,7 +67,7 @@
 			var currentDate = new Date(year, month, 1);
 			
 			
-			var firstDayOffset = locale.firstDayOfWeek - currentDate.getDay() + 1;
+			var firstDayOffset = Date.firstDayOfWeek - currentDate.getDay() + 1;
 			if (firstDayOffset > 1) firstDayOffset -= 7;
 			currentDate.addDays(firstDayOffset-1);
 			
@@ -114,7 +98,375 @@
 					$(this).empty().append(calendarTable);
 				}
 			);
+		},
+		
+		datePicker : function(s)
+		{			
+			if (!$.event._dpCache) $.event._dpCache = [];
+			
+			// initialise the date picker controller with the relevant settings...
+			s = $.extend(
+				{
+					month				: undefined,
+					year				: undefined,
+					startDate			: undefined,
+					endDate				: undefined,
+					renderCallback		: [],
+					createButton		: true,
+					showYearNavigation	: true,
+					closeOnSelect		: true
+				}
+				, s
+			);
+			
+			return this.each(
+				function()
+				{
+					$this = $(this);
+					
+					if (!this._dpId) {
+						this._dpId = $.event.guid++;
+						$.event._dpCache[this._dpId] = new DatePicker(this);
+					}
+					
+					var controller = $.event._dpCache[this._dpId];
+					
+					controller.init(s);
+					
+					if (s.createButton) {
+						// create it!
+					}
+					
+				}
+			)
+		},
+		
+		dpSetStartDate : function(d)
+		{
+			return _w.call(this, 'setStartDate', d);
+		},
+		dpSetEndDate : function(d)
+		{
+			return _w.call(this, 'setEndDate', d);
+		},
+		dpSetDisplayedMonth : function(m, y)
+		{
+			return _w.call(this, 'setDisplayedMonth', Number(m), Number(y));
+		},
+		dpDisplay : function()
+		{
+			return _w.call(this, 'display');
+		},
+		dpSetRenderCallback : function(a)
+		{
+			return _w.call(this, 'setRenderCallback', a);
+		},
+		// private function called on unload to clean up any expandos etc and prevent memory links...
+		_dpDestroy : function()
+		{
+			// TODO - implement this?
 		}
 	});
+	
+	// private internal function to cut down on the amount of code needed where we forward
+	// wp* methods on the jQuery object on to the relevant DatePicker controllers...
+	var _w = function(f, a)
+	{
+		return this.each(
+			function()
+			{
+				var c = _getController(this);
+				if (c) {
+					c[f](a);
+				}
+			}
+		);
+	}
+	
+	function DatePicker(ele)
+	{
+		this.ele = ele;
+	}
+	$.extend(
+		DatePicker.prototype,
+		{
+			displayedMonth		:	null,
+			displayedYear		:	null,
+			startDate			:	null,
+			endDate				:	null,
+			renderCallback		:	[],
+			showYearNavigation	:	true,
+			closeOnSelect		:	true,
+			
+			init : function(s)
+			{
+				this.setStartDate(s.startDate);
+				this.setEndDate(s.endDate);
+				this.setDisplayedMonth(Number(s.month), Number(s.year));
+				this.setRenderCallback(s.renderCallback);
+				this.showYearNavigation = s.showYearNavigation;
+				this.closeOnSelect = s.closeOnSelect;
+			},
+			setStartDate : function(d)
+			{
+				if (d) {
+					this.startDate = Date.fromString(d);
+				}
+				if (!this.startDate) {
+					this.startDate = (new Date()).zeroTime();
+				}
+			},
+			setEndDate : function(d)
+			{
+				if (d) {
+					this.endDate = Date.fromString(d);
+				}
+				if (!this.endDate) {
+					this.endDate = (new Date('12/31/2999')); // using the JS Date.parse function which expects mm/dd/yyyy
+				}
+				if (this.endDate.getTime() < this.startDate.getTime()) {
+					this.endDate = this.startDate;
+				}
+			},
+			setDisplayedMonth : function(m, y)
+			{
+				var s = new Date(this.startDate.getTime());
+				s.setDate(1);
+				var e = new Date(this.endDate.getTime());
+				e.setDate(1);
+				
+				var t;
+				
+				if (isNaN(m) && isNaN(y)) {
+					// no month or year passed - default to current month
+					t = new Date().zeroTime();
+					t.setDate(1);
+				} else if (isNaN(m)) {
+					// just year passed in - presume we want the displayedMonth
+					t = new Date(y, this.displayedMonth, 1);
+				} else if (isNaN(y)) {
+					// just month passed in - presume we want the displayedYear
+					t = new Date(this.displayedYear, m, 1);
+				} else {
+					// year and month passed in - that's the date we want!
+					t = new Date(y, m, 1)
+				}
+				
+				// check if the desired date is within the range of our defined startDate and endDate
+				if (t.getTime() < s.getTime()) {
+					t = s;
+				} else if (t.getTime() > e.getTime()) {
+					t = e;
+				}
+				this.displayedMonth = t.getMonth();
+				this.displayedYear = t.getFullYear();
+			},
+			display : function()
+			{
+				var c = this;
+				var $ele = $(this.ele);
+				var eleOffset = $ele.offset();
+				
+				
+				var _checkMouse = function(e)
+				{
+					var el = e.target;
+					var cal = $('#dp-popup')[0];
+					while (true){
+						if (el == cal) {
+							return true;
+						} else if (el == document) {
+							c._closeCalendar();
+							return false;
+						} else {
+							el = $(el).parent()[0];
+						}
+					}
+				};
+				this._checkMouse = _checkMouse;
+				
+				this._closeCalendar();
+				
+				$('body')
+					.append(
+						$('<div></div>')
+							.attr('id', 'dp-popup')
+							.css(
+								{
+									'top'	:	eleOffset.top,
+									'left'	:	eleOffset.left
+								}
+							)
+							.append(
+								$('<h2></h2>'),
+								$('<div id="dp-nav-prev"></div>')
+									.append(
+										$('<a id="dp-nav-prev-year" href="#" title="' + $.dpText.TEXT_PREV_YEAR + '">&lt;&lt;</a>')
+											.bind(
+												'click',
+												function()
+												{
+													return c._displayNewMonth.call(c, this, 0, -1);
+												}
+											),
+										$('<a id="dp-nav-prev-month" href="#" title="' + $.dpText.TEXT_PREV_MONTH + '">&lt;</a>')
+											.bind(
+												'click',
+												function()
+												{
+													return c._displayNewMonth.call(c, this, -1, 0);
+												}
+											)
+									),
+								$('<div id="dp-nav-next"></div>')
+									.append(
+										$('<a id="dp-nav-next-year" href="#" title="' + $.dpText.TEXT_NEXT_YEAR + '">&gt;&gt;</a>')
+											.bind(
+												'click',
+												function()
+												{
+													return c._displayNewMonth.call(c, this, 0, 1);
+												}
+											),
+										$('<a id="dp-nav-next-month" href="#" title="' + $.dpText.TEXT_NEXT_MONTH + '">&gt;</a>')
+											.bind(
+												'click',
+												function()
+												{
+													return c._displayNewMonth.call(c, this, 1, 0);
+												}
+											)
+									),
+								$('<div></div>')
+									.attr('id', 'dp-calendar')
+							)
+						);
+				if (this.showYearNavigation == false) {
+					$('#dp-nav-prev-year, #dp-nav-next-year').css('display', 'none');
+				}
+				c._renderCalendar();
+				$(document).bind('mousedown', this._checkMouse);
+			},
+			setRenderCallback : function(a)
+			{
+				if (a && typeof(a) == 'function') {
+					a = [a];
+				}
+				this.renderCallback = this.renderCallback.concat(a);
+			},
+			cellRender : function ($td, thisDate, month, year) {
+				var c = this.dpController;
+				var d = new Date(thisDate.getTime());
+				
+				// add our click handlers to deal with it when the days are clicked...
+				
+				$td.bind(
+					'click',
+					function()
+					{
+						$(c.ele).trigger('dateSelected', [d, $td]);
+						if (c.closeOnSelect) {
+							c._closeCalendar();
+						}
+					}
+				);
+				
+				// call any extra renderCallbacks that were passed in
+				for (var i=0; i<c.renderCallback.length; i++) {
+					c.renderCallback[i].apply(this, arguments);
+				}
+				
+				
+			},
+			// ele is the clicked button - only proceed if it doesn't have the class disabled...
+			// m and y are -1, 0 or 1 depending which direction we want to go in...
+			_displayNewMonth : function(ele, m, y) 
+			{
+				if (!$(ele).is('.disabled')) {
+					this.setDisplayedMonth(this.displayedMonth + m, this.displayedYear + y);
+					this._clearCalendar();
+					this._renderCalendar();
+				}
+				ele.blur();
+				return false;
+			},
+			_renderCalendar : function()
+			{
+				// set the title...
+				$('#dp-popup h2').html(Date.monthNames[this.displayedMonth] + ' ' + this.displayedYear);
+				
+				// render the calendar...
+				$('#dp-calendar').renderCalendar(
+					{
+						month			: this.displayedMonth,
+						year			: this.displayedYear,
+						renderCallback	: this.cellRender,
+						dpController	: this
+					}
+				);
+				
+				// update the status of the control buttons...
+				// TODO: When should the year buttons be disabled? When you can't go forward a whole year from where you are or is that annoying?
+				if (this.displayedYear == this.startDate.getFullYear() && this.displayedMonth == this.startDate.getMonth()) {
+					$('#dp-nav-prev-year').addClass('disabled');
+					$('#dp-nav-prev-month').addClass('disabled');
+				} else {
+					$('#dp-nav-prev-year').removeClass('disabled');
+					$('#dp-nav-prev-month').removeClass('disabled');
+				}
+				if (this.displayedYear == this.endDate.getFullYear() && this.displayedMonth == this.endDate.getMonth()) {
+					$('#dp-nav-next-year').addClass('disabled');
+					$('#dp-nav-next-month').addClass('disabled');
+				} else {
+					$('#dp-nav-next-year').removeClass('disabled');
+					$('#dp-nav-next-month').removeClass('disabled');
+				}
+				
+			},
+			_closeCalendar : function()
+			{
+				$(document).unbind('mousedown', this._checkMouse);
+				this._clearCalendar();
+				$('#dp-popup').empty().remove();
+			},
+			// empties the current dp-calendar div and makes sure that all events are unbound
+			// and expandos removed to avoid memory leaks...
+			_clearCalendar : function()
+			{
+				// TODO.
+				$('#dp-calendar').empty();
+			}
+		}
+	);
+	
+	// static constants
+	$.dpConst = {
+		SHOW_HEADER_NONE	:	0,
+		SHOW_HEADER_SHORT	:	1,
+		SHOW_HEADER_LONG	:	2
+	}
+	$.dpText = {
+		TEXT_PREV_YEAR		:	'Previous year',
+		TEXT_PREV_MONTH		:	'Previous month',
+		TEXT_NEXT_YEAR		:	'Next year',
+		TEXT_NEXT_MONTH		:	'Next month'
+	}
 
+	function _getController(ele)
+	{
+		if (ele._dpId) return $.event._dpCache[ele._dpId];
+		return false;
+	}
+
+
+	// clean-up
+	$(window)
+		.bind('unload', function() {
+			var els = $.event._dpCache || [];
+			for (var i in els) {
+				$(els[i].ele)._dpDestroy();
+			}
+		});
+		
+	
 })(jQuery);

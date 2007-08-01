f6fe9a128ddf7a29d8c56cc14c2aeba2d78f494f
digitalspaghetti <digitalspaghetti@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-01 16:49:43 +0000

    Started work on plugin proper

--- branches/tane-dev/ui.menu.js
+++ branches/tane-dev/ui.menu.js
@@ -1,44 +1,80 @@
 /* jQuery UI Menu
  * 
  * m = menu being passed in
- * h = handler types
- * d = display effect
  * o = options
- * f = trigger functions
+ * t = trigger functions
  */
 
 (function($){
 	
-	$.fn.menu = function(m, h, d, o, f) {
+	var menuItems = [];	// This is the array we will store the menu items in
+	
+	$.fn.clickContext = function(m,o,t) {	// Constructor for the clickContext method
 		return this.each(function() {
-			new $.ui.menu(this, m, h, d, o, f);	
+			new $.ui.clickContext(this, m, o, t);	
+		});
+	}
+	$.fn.hoverContext = function(m,o,t) {	// Constructor for the hoverContext method
+		return this.each(function() {
+			new $.ui.hoverContext(this, m, o, t);	
+		});
+	}
+	$.fn.context = function(m,o,t) {	// Constructor for the context method
+		return this.each(function() {
+			new $.ui.context(this, m, o, t);	
+		});
+	}
+	$.fn.hideContext = function(m,o,t) {	// Constructor for the closeContext method
+		return this.each(function() {
+			new $.ui.hideContext(this, m, o, t);	
 		});
 	}
 	
 	
-	$.ui.menu = function(el, m, h, d, o, f) {
+	$.ui.clickContext = function(el, m, o, t) {
+		// Keys for possible alt functionality
+		var ALT = false;
+		var CTRL = false;
+		var SHIFT = false;
+		
+		var self = el;
+		
+		$(self).bind('click', function(){
+			$(m).slideDown('slow', function(){
+				$(self).unbind('click').bind('click', function(){
+					$(m).slideUp('slow').unbind('click');
+				});
+			});
+		});
+			
+	}
+	$.ui.hoverContext = function(el, m, o, t) {
+		// Keys for possible alt functionality
+		var ALT = false;
+		var CTRL = false;
+		var SHIFT = false;
+		
+		var self = el;
+		
+		$(self).bind('mouseover', function(){
+			$(m).slideDown('slow', function(){
+				$(self).bind('mouseout', function(){
+					$(m).slideUp('slow');
+				});
+			});
+		});
+			
+	}
+	$.ui.context = function(el, m, o, t) {
 		// Keys for possible alt functionality
 		var ALT = false;
 		var CTRL = false;
 		var SHIFT = false;
 			
-		var attach = el;
-		var menu = m;
-		var handler = h;
-		var display = d;
-		var functions = f;
-		
-		var options = {};
-		$.extend(options, o);
-		$.extend(options, {
-		
-		});
-		
-		$(attach).bind(handler, function(){
-			$(menu).toggle();
-		})
-		
-	};
+	}
+	$.ui.hideContext = function(el, m, o, t) {
+			
+	}
 	
 	
 })($);

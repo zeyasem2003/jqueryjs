1dae4bfc0979465ccd1d5b839d3f8372f1524aac
digitalspaghetti <digitalspaghetti@c715fcbe-d12f-0410-84c4-316a508785bb>
2007-08-08 21:25:49 +0000

    Added hideMenu function, test bound to window click

--- branches/tane-dev/uimenu/ui.menu.js
+++ branches/tane-dev/uimenu/ui.menu.js
@@ -22,7 +22,9 @@
 		
 		var options = $.extend({
 			timeout: 2000,
-			bindto: 'click'
+			bindto: 'click',
+			anim: 'show',
+			speed: 'slow'
 		}, o);
 		var buttons = $.extend({}, t);
 
@@ -31,14 +33,17 @@
 		var SHIFT = false;
 		
 		this.styleMenu(menu);
-		
+		var v = this;
 		$(attach).bind(options.bindto, function(){
 			x = $(attach).position();
 			elBottom = x.top + $(attach).height();
 			elLeft = x.left;
 			$(m).css({position:'absolute', top:elBottom + 1, left: elLeft})
-			$(m).show('slow', function(){
-				console.log('Menu Shown')
+			$(m)[options.anim](options.speed, function(){
+				console.log('Menu Shown');
+				$(window).bind('click', function(){
+					v.hideMenu(m, options);
+				})
 			});
 		});
 	}
@@ -51,6 +56,22 @@
 				.css('MozUserSelect', 'none').attr('unselectable', 'on');
 			var node = $('li',m).addClass('ui-menu-node')
 				.css('MozUserSelect', 'none').attr('unselectable', 'on');
+			return false;
+		},
+		hideMenu : function(m, o){
+			
+			switch (o.anim) {
+				case "show" :
+					var alt = "hide";
+					break;
+				case "slideDown":
+					var alt = "slideUp";
+					break;
+				case "fadeIn" :
+					var alt = "fadeOut";
+					break;
+			}
+			$(m)[alt](o.speed);
 			return false;
 		}
 		

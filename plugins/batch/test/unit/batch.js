0555dfb2417431404b4f9da782b28af314a16985
jeresig <jeresig@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-02-06 05:21:25 +0000

    Tagging the 1.2.3 release.

--- plugins/batch/test/unit/batch.js
+++
@@ -1,6 +0,0 @@
-module("batch");
-
-test("batch", function() {
-	isSet( $('input[value=Test]').attrs('value'), ["Test", "Test"], "$('input[value=Test]').attrs('value')" );
-	isSet( $('input[value=Test]').attrs('value', function(){ return 'Updated'; }).attrs('value'), ["Updated", "Updated"], "$('input[value=Test]').attrs('value', function(){ return 'Updated'; })" );
-});

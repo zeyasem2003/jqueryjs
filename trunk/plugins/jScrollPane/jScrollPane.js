63f58e90e31a26d40304d8fdb45a14e55bcc496a
kelvinluck <kelvinluck@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-11-06 23:35:32 +0000

    jScrollPane - much cleaner fix for tabbing to elements within a jScrollPane...

--- trunk/plugins/jScrollPane/jScrollPane.js
+++ trunk/plugins/jScrollPane/jScrollPane.js
@@ -397,22 +397,13 @@ jQuery.fn.jScrollPane = function(settings)
 				
 				scrollTo(-currentScrollPosition, true);
 			
-				$('input, a', this).bind(
+				// Deal with it when the user tabs to a link or form element within this scrollpane
+				$('input, a, select', this).bind(
 					'focus',
 					function()
 					{
-						// We need to wait a millisecond so that any scroll triggered by the link has happened before we are checking it...
-						// Luckily we have the closure to keep track of all of our vars...
-						setTimeout(function()
-							{
-								//console.log('In TO', $this.offset().top, $this.position().top);
-								if ($this.position().top < 0) {
-									var diff = $track.position().top;
-									//console.log(diff);
-									$container.scrollTop(0);
-									$this[0].scrollTo(-diff + 30 - (parseInt($pane.css('top')) || 0), true);
-								}
-							}, 0);
+						$container.scrollTop(0);
+						scrollTo($(this).position().top - settings.scrollbarMargin , true);
 					}
 				)
 				

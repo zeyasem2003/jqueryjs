1b7833e09daa7c3eb50c0d5ed9bb31f7e41e9918
jeresig <jeresig@c715fcbe-d12f-0410-84c4-316a508785bb>
2008-12-22 17:31:22 +0000

    Tagging the 1.3b1 release.

--- tools/rails_demo_app/vendor/plugins/jquery_on_rails/js/jquery_modules/tabs.js
+++
@@ -1,9 +0,0 @@
-$.meta.setType("class");
-jQuery(function($) {
-  $("div.tab_container").each(function() {
-    var data = $(this).data();
-    var start = data.start;
-    delete data.start;
-    $(this).tabs(start, data);
-  })
-})

7659f5deacb2379eea7ac192c670a8d3b226838c
jeresig <jeresig@c715fcbe-d12f-0410-84c4-316a508785bb>
2009-01-13 17:50:31 +0000

    Tagging the 1.3 release.

--- plugins/ajaxFilter/jquery.ajaxFilter-min.js
+++
@@ -1,9 +0,0 @@
-/**
- * jQuery.AjaxFilter - Registry of filters for AJAX responses.
- * Copyright (c) 2008 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
- * Dual licensed under MIT and GPL.
- * Date: 8/7/2008
- * @author Ariel Flesler
- * @version 1.0.1
- */
-;(function($){var b={};$.ajaxFilter={f:b,register:function(c,f,g){$.each(f.split(' '),function(d,a){if(!b[a])b[a]={};b[a][c]=g})}};$.ajaxSettings.dataFilter=function(d,a){var c=b[a]&&b[a][this.filter];return c?c.call(this,d,a):d}})(jQuery);

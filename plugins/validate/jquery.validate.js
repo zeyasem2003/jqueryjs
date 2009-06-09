86b6c28d2e5ef359718f3e0e06441f2ded27892b
joern.zaefferer <joern.zaefferer@c715fcbe-d12f-0410-84c4-316a508785bb>
2009-06-09 16:46:05 +0000

    validate: fixed line delimiters and a missing semicolon

--- plugins/validate/jquery.validate.js
+++ plugins/validate/jquery.validate.js
@@ -18,13 +18,13 @@
 $.extend($.fn, {
 	// http://docs.jquery.com/Plugins/Validation/validate
 	validate: function( options ) {
-		
+
 		// if nothing is selected, return nothing; can't chain anyway
 		if (!this.length) {
 			options && options.debug && window.console && console.warn( "nothing selected, can't validate, returning nothing" );
 			return;
 		}
-		
+
 		// check if a validator for this form was already created
 		var validator = $.data(this[0], 'validator');
 		if ( validator ) {
@@ -196,7 +196,7 @@ $.validator.format = function(source, params) {
 		source = source.replace(new RegExp("\\{" + i + "\\}", "g"), n);
 	});
 	return source;
-}
+};
 
 $.extend($.validator, {
 	
